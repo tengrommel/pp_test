cb3ff91e4671d9bf6a0afec1fcb617be7749aade
tengrommel <wwwsousouccom@126.com>
2017-12-19 15:53:10 +0800

    ok

--- data_python/Python_numpy.py
+++ data_python/Python_numpy.py
@@ -9,17 +9,19 @@ import numpy as np
 """
 演示numpy速度
 """
+
+# 两个numpy数组相加
 def numpysum(n):
     a = np.arange(n)**2
     b = np.arange(n)**3
     c = a+b
     return c
 
+# 两个列表相加
 def pythonsum(n):
     a = range(n)
     b = range(n)
     c = []
-
     for i in range(len(a)):
         a[i] = i ** 2
         b[i] = i ** 3
@@ -31,7 +33,6 @@ c = pythonsum(size)
 delta = datetime.now()-start
 print "The last 2 elements of the sum", c[-2:]
 print "PythonSum elapsed time in microseconds", delta.microseconds
-
 start = datetime.now()
 c = numpysum(size)
 delta = datetime.now() - start

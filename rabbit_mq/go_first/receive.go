48d1b78b0d77f2de26fb0e3c5f542d42a00acb7d
tengrommel <wwwsousouccom@126.com>
2018-01-03 14:15:50 +0800

    ok

--- rabbit_mq/go_first/receive.go
+++ rabbit_mq/go_first/receive.go
@@ -21,34 +21,24 @@ func main()  {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"hello",// name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		"",		// exchange
+		true,	// auto-ack
+		false,	// exclusive
+		false,	// no-local
+		false,	// no-wait
+		nil,	// args
 	)
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
